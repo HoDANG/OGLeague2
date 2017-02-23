74abadc4cf1fd25a24b6f7ee1a4e2e7310ef6c93
moonshadow565 <moonshadow565@gmail.com>
2017-02-23 15:41:33 +0100

    Testing stats

--- main.cpp
+++ main.cpp
@@ -11,6 +11,21 @@ string aName = "Test";
 int aSkin = 0;
 int aMap = 0;
 
+uint32_t syncID = 1;
+uint32_t syncID2 = 1;
+
+void pprintPacket(uint8_t *data, size_t size)
+{
+    std::cout<<std::hex;
+    for(int i=0;i<size;i++)
+    {
+        if(i != 0  && i%8 == 0) std::cout<<std::endl;
+        if(data[i] < 16) std::cout<<0;
+        std::cout<<(uint32_t) data[i]<<" ";
+    }
+    std::cout<<std::dec<<std::endl;
+}
+
 class BasicClient : public NetClient
 {
 public:
@@ -44,8 +59,8 @@ public:
         strcpy(ans.mVersionString , req->mVersionString);
         ans.playerInfo[0].ID = cid;
         ans.playerInfo[0].summonorLevel = 30;
-        ans.playerInfo[0].summonorSpell1 = 0;
-        ans.playerInfo[0].summonorSpell2 = 0;
+        ans.playerInfo[0].summonorSpell1 = 0x06496EA8;
+        ans.playerInfo[0].summonorSpell2 = 0x06364F24;
         server->sendPacket(cid, (uint8_t*) &ans, sizeof(ans));
     }
 
@@ -87,7 +102,7 @@ public:
         for(int i=0;i<13;i++)
             ans2 < (uint8_t) 0;              //unk
         ans2 < (uint8_t) 3                  //type
-        < (uint32_t) 1                      //syncID
+        < (uint32_t) syncID2++                     //syncID
         < (float) 26.0f < (float) 280.0f    //x y
         < (float) 26.0f < (float) 280.0f;   //x y
         server->sendPacket(cid, ans2.data(), ans2.size());
@@ -130,6 +145,63 @@ public:
     void OnPayload(uint32_t cid, EGP_Chat_s *payload, size_t size)
     {
         server->sendPacket(cid, (uint8_t*) payload, size, CHANNEL_MIDDLE_TIER_CHAT);
+        string chat((char*) &payload->buffer[0]);
+        stringstream in;
+        string key;
+        in<<chat;
+        in>>key;
+        if(key == "s")
+        {
+            uint32_t mastern;
+            uint32_t fieldn;
+            string type;
+            uint32_t val = 0;
+            in>>mastern;
+            in>>fieldn;
+            in>>type;
+            if(type == "f")
+                in>>*((float*)&val);
+            if(type == "i")
+                in>>*((int32_t*)&val);
+            if(type == "u")
+                in>>val;
+
+            stringstream buff;
+            PKT_OnReplication_s rep;
+            rep.syncID = syncID++;
+            rep.count = 1;
+            rep.fromID = 0;
+            RepHeader repheader;
+            repheader.networkID = 0x40000001;
+            repheader.setMaps = ((uint8_t)1 << (mastern-1));;
+            buff<rep;
+            buff<repheader;
+            buff<((uint32_t)1 << (fieldn-1));
+            buff<val;
+            string tmp = buff.str();
+            server->sendPacket(cid, (uint8_t*) tmp.c_str(), tmp.size());
+        }else if(key == "m")
+        {
+            float x1,y1, x2, y2;
+            in>>x1>>y1>>x2>>y2;
+            DynamicPacket<PKT_OnEnterVisiblityClient_s> ans2;
+            ans2.base()->fromID = 0x40000001;
+            for(int i=0;i<13;i++)
+                ans2 < (uint8_t) 0;
+            ans2 < (float)1.0f;
+            for(int i=0;i<13;i++)
+                ans2 < (uint8_t) 0;
+            ans2 < (uint8_t) 3
+            < (uint32_t) syncID2++
+            < x1 <  y1    //x y
+            < x2  < y2;   //x y
+            server->sendPacket(cid, ans2.data(), ans2.size());
+        }
+    }
+
+    void OnPacket(uint32_t cid, PKT_NPC_IssueOrderReq_s *req, size_t size)
+    {
+        pprintPacket((uint8_t*) req, size);
     }
 
     //Spam... xD
@@ -138,19 +210,8 @@ public:
 
 
 
-
 int main(int argc, char ** argv)
 {
-
-    if(argc > 1)
-        aMap = stoi(argv[1]);
-    if(argc > 2)
-        aChampion = argv[2];
-    if(argc > 3)
-        aSkin = stoi(argv[3]);
-    if(argc > 4)
-        aName = argv[4];
-
     enet_initialize();
     auto client = new BasicClient;
     server = new NetServer(client, ENET_HOST_ANY, 5119, "GLzvuWtyCfHyGhF2", 12);
--- net/pkt/020_PKT_NPC_IssueOrderReq.hpp
+++ net/pkt/020_PKT_NPC_IssueOrderReq.hpp
@@ -1,10 +1,30 @@
 #ifndef HPP_020_PKT_NPC_IssueOrderReq_HPP
 #define HPP_020_PKT_NPC_IssueOrderReq_HPP
-#include "../base.h"
 
+#include "../base.h"
 #pragma pack(push, 1)
 struct PKT_NPC_IssueOrderReq_s : DefaultPacket<PKT_NPC_IssueOrderReq>
 {
+/*
+14
+01 00 00 40
+02          order
+0c b2 3d 44 posX
+d8 63 3a 43 posY
+f2 23 8a 43 posZ
+00 00 00 00 targetNetID
+b0 ef 03 00
+01 00 00 40
+0b 76 f2 f7
+f1 64 32 bb
+f3 32
+*/
+    char order;
+    float posX;
+    float posY;
+    float posZ;
+    unsigned int targetNetID;
+    uint8_t data[];
 };
 #pragma pack(pop)
 
